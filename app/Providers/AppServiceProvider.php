f544364be2848e8dc65ff4c0da402eeab9138448
pradytia <pradytia@mobilecom.co.id>
2020-08-09 22:23:16 +0700

    setting appProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(250);
+
+        if(env('APP_ENV') !== 'local'){
+            URL::forceScheme('https');
+        }
     }
 }
