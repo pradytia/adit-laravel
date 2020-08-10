0fb00e811d54f218f09f912138c727c957ec55d9
pradytia <pradytia@mobilecom.co.id>
2020-08-10 08:01:06 +0700

    edit responsive jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,37 +8,25 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
 	<style>
-        /*untuk teks heading di tag <body>*/
-        h2
-        {
-            background-color: white;
-            color: crimson;
-            font-family: sans-serif;
-            text-align: center;
-            width: 45%;
-            margin:auto;
-            padding: 20px;
-        }
  
         body
         {
-            background-image: url('https://i.stack.imgur.com/vhoa0.jpg');
-            background-repeat: no-repeat;
+            background: url('https://i.stack.imgur.com/vhoa0.jpg') no-repeat center center fixed; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
             background-size: cover;
         }
     </style>
 </head>
 <body>
-<div class="container mt-5 ">			
-		<!-- membuat jumbotron -->
-		<div class="jumbotron py-5">
-			<center>			
-				<h2>Selamat datang di Toko Kelontong</h2>
-				<p>Untuk melanjutkan silahkan login</p><br/><br/>
-				<p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a></p>
-			</center>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <h2>Selamat datang di Toko Kelontong</h2>
+            <p>Untuk melanjutkan silahkan login</p>
+            <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a></p>
         </div>
-</div>
+    </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
