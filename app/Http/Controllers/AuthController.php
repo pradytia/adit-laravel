e15e926d3d0453572378d571dc0f4318cdaf43f9
pradytia <pradytia@mobilecom.co.id>
2020-08-10 07:26:19 +0700

    edit statement format id unique

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,11 +34,11 @@ class AuthController extends Controller
 
     function postregister(Request $request){
 
-        // $now = Carbon::now();
-        // $unique_code = $now->format('YmdHisu');
+        $now = Carbon::now();
+        $unique_code = $now->format('YmdHisu');
 
         $user = new \App\User;
-        $user->id = '2';
+        $user->id = $unique_code;
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
